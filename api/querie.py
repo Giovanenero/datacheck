fb3268b148dd7f0ffd9b4198505ecdc7a3bce6d4
Carlo Moro <carlo.moro@wises.com.br>
2025-10-02 14:11:34 +0000

    ajustando nginx e requirements

--- api/querie.py
+++ api/querie.py
@@ -13,6 +13,8 @@ MAX_RETRIES = 40
 def get_driver():
     options = Options()
     options.add_argument('--headless')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Firefox(options=options)
 
     return driver
